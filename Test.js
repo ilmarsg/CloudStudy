4afd66d510ae15ab26881765ff18b79cb0a16264
Ilmārs Geiba <ilmars.geiba@gmail.com>
2022-12-03 01:29:13 +0200

    Dienas beigas

--- Test.js
+++ Test.js
@@ -1,2 +1,8 @@
-//write hello world in console
-console.log("Hello World");
+let re1 = new RegExp("abc");
+let re2 = /abc/;
+console.log(/abc/.test("abcde")); //
+// → true
+console.log(/abc/.test("abxde"));
+// → false
+console.log(/[0-9]/.test("in 1992"));
+// → true
