bff09f6820794ad5e4a21c91a9d0ed88042cf80b
Ilmārs Geiba <ilmars.geiba@gmail.com>
2022-12-15 20:05:18 +0200

    Pabeigts 6. nedēļas uzdevums

--- 6neduzd/index.js
+++ 6neduzd/index.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
-const {parse} = require("csv-parse/sync");
-const csv = require("csv-parser");
+const {parse} = require("csv-parse");
 
-app.get('/getData/make/:make/engine/:engine', function (req, res) {
-    const regex = new RegExp(".*" + req.params.make + ".*" + req.params.engine + ".*", "gi");
-    const results = [];
-    fs.createReadStream(__dirname + "/" + "data.txt")
-    .pipe(csv({separator: ";", filter: (line) => regex.match(line)}))
-    .on('data', (line) => results.push(line))
-    .on('end', () => {
-        res.end(JSON.stringify(results));
-    });
+app.get("/getData/make/:make/engine/:engine", function (req, res) {
+    let result = [];
+    fs.createReadStream(__dirname + "/" + "ta2017c.lst")
+        .pipe(parse({delimiter: ";", from_line: 2, relax_column_count: true}))
+        .on("data", function (row) {
+            let NormMake = req.params["make"].normalize("NFD").replace(/\p{Diacritic}/gu, "");
+            let NormEngine = req.params["engine"].normalize("NFD").replace(/\p{Diacritic}/gu, "");
+            let RegExMake = new RegExp(NormMake, "i");
+            let RegExEngine = new RegExp(NormEngine, "i");
+            let NormRow1 = "";
+            let NormRow2 = "";
+            if (row[1] != null) {
+                NormRow1 = row[1].normalize("NFD").replace(/\p{Diacritic}/gu, "");
+            }
+            if (row[2] != null) {
+                NormRow2 = row[2].normalize("NFD").replace(/\p{Diacritic}/gu, "");
+            }
+            if (RegExMake.test(NormRow1) && RegExEngine.test(NormRow2)) {
+                var entry = {
+                    "date": row[0],
+                    "car": row[1],
+                }
+                result.push(entry);
+            }
+        })
+        .on("end", () => {
+            res.end(("{ \"results\": " + JSON.stringify(result) + ", \"count\": " + result.length + " }"));
+        })
+        .on("error", (err) => {
+            res.end("Kļūda: " + err);
+        })
 })
 
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Lietotne ieslēgta un klausās: http://%s:%s", host, port)
+    console.log("Tīmekļa pakalpe ir ieslēgta un klausās adresē: http://%s:%s", host, port)
 })
